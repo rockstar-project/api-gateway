46df0d533eaf14c9df3bd6632c8b076d70e7a828
Sal Siddiqui <salsiddiqui0@gmail.com>
2018-02-09 23:45:20 -0600

    updated

--- src/main/java/com/rockstar/cloud/gateway/GatewayApplication.java
+++ src/main/java/com/rockstar/cloud/gateway/GatewayApplication.java
@@ -2,7 +2,6 @@ package com.rockstar.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
@@ -22,7 +21,7 @@ public class GatewayApplication  {
     }
     
     @Bean
-    public FilterRegistrationBean corsFilter() {
+    public CorsFilter corsFilter() {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
@@ -37,9 +36,7 @@ public class GatewayApplication  {
         config.addAllowedMethod("DELETE");
         config.addAllowedMethod("PATCH");
         source.registerCorsConfiguration("/**", config);
-        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-        bean.setOrder(0);
-        return bean;
+        return new CorsFilter(source);
     }
     
 }
